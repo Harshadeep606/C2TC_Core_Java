86c0c91459d25b4c688d3af8fcc3898fa8534123
SWAPNIL <SWAPNIL@iamjarvis>
2022-11-03 18:45:06 +0530

    First Commit

---
+++ JavaProgramsDemo/src/com/capgemini/javademo/PrimitiveDatatypes.java
@@ -0,0 +1,20 @@
+// This program demonstrates the different primitive data types in java
+
+package com.capgemini.javademo;
+
+public class PrimitiveDatatypes {
+
+	public static void main(String[] args) {
+		
+		int maximumInteger = Integer.MAX_VALUE;
+		int minimimInteger = Integer.MIN_VALUE;
+		short shortTypeMax = 32767;
+		short shortTypeMin = -32768;
+		long maximumLongInteger = 9223372036854775807L;
+		long minimumLongInteger = -9223372036854775808L;
+		byte maximumByteInteger = 127;
+		byte minimumByteInteger = -128;
+
+	}
+
+}
